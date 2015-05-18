5673d3d1a35e3296a3aabd0bed1583463d12898d
Kacper <random8902@gmail.com>
2015-05-18 04:01:58 +0200

    Break or not

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,8 +1,24 @@
-from django.test import TestCase
+from django.core.urlresolvers import resolve
+from django import TestCase
+from django.http import HttpRequest
+
+from lists.views import home_page
 
 # Create your tests here.
 
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
 
-    def test_bad_maths(self):
-        self.assertEqual(1+1,3)
+    def test_root_url_resolve_to_home_page_view(self):
+        found = resolve ('/')
+        self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        
+        self.assertTrue(response.content.startwith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.context.endswith(b'</html>'))
+
+
+
